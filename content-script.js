5b0159067c21426a2f4bf6df4a387c3690d765b0
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-06-25 18:06:52 +0200

    refactor: listen only on document click events

--- content-script.js
+++ content-script.js
@@ -11,59 +11,57 @@ const log = (level, msg) => {
 	}
 }
 
-
 const startWithProtoRegex = new RegExp(/^https?:\/\//);
 const startWithOutProto = new RegExp(/^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/);
 
-function warn(e){
-	const a = e.currentTarget;
-	if( a.hasAttribute('href') ){
-		if (  startWithProtoRegex.test(a.href) ) {
-			const a_text = a.text.trimStart().split(' ')[0];
-			const href_url = new URL(a.href);
-			const href_protocol = href_url.protocol;
-			if (  (startWithProtoRegex.test(a_text) && a.href !== a_text)
-				||  (startWithOutProto.test(a_text) && ( (href_protocol +'//' + a_text) !== a.href))
-			) { 
-				const result = confirm('WARNING: LINK TEXT DOES NOT MATCH TARGET!!!\n\nLink Text: "'+ a.text + '" \nLink Target: "' + a.href +'" \n\n Click (OK) to continue or (Cancel) to stop navigation');
-				if(!result){
-					e.preventDefault();
-					return false;
-				}
-			}
+function warn(e) {
+	if(typeof e.target === 'undefined') {
+		log('debug', 'event target is undefined');
+		return;
+	}
+	const el = e.target;
 
+	if( el.nodeName.toLowerCase() !== 'a') {
+		log('debug','clicked element is not a anchor');
+		return;
+	}
+	if( !el.hasAttribute('href') ){
+		log('debug','link has no href attribute');
+		return;
+	}
+	if (  !startWithProtoRegex.test(el.href) ) {
+		log('debug','href doenst have a valid protocol');
+		return;
+	}
+
+	const el_text = el.text.trimStart().split(' ')[0];
+	const href_url = new URL(el.href);
+	const href_protocol = href_url.protocol;
+
+	if (       (startWithProtoRegex.test(el_text) && el.href !== el_text)
+		|| (startWithOutProto.test(el_text) && ( (href_protocol +'//' + el_text) !== el.href))
+	) { 
+		const result = confirm(
+			'WARNING: Link Text and Target dont match!\n' + 
+			'\n' +
+			'Text: "'+ el.text + '" \n' + 
+			'Target: "' + el.href +'"\n' + 
+			'\n' +
+			'Click (OK) to continue or (Cancel) to stop' +
+			'\n'
+		);
+		if(!result){
+			e.preventDefault();
+			return false;
 		}
 	}
+
+	if(temporary) {
+		e.preventDefault();
+		return false;
+	}
 	return true;
-	/* */
-	//e.preventDefault();return false;
-	/* */
 }
 
-function addWarnToNewAnchors(mutationList, observer){
 
-const as = document.getElementsByTagName("a");
-for (const a of as) {
-	a.removeEventListener("click", warn, false);
-	a.addEventListener("click", warn, false);
-}
-	/*
-	mutationList.forEach( (mutation) => {
-		if (mutation.type === 'childList') {
-			mutation.addedNodes.forEach( (aNode) => {
-				if(aNode.nodeName.toLowerCase() === 'a') {
-					log('debug', 'added eventlistener');
-					aNode.addEventListener("click", warn, false);
-				}
-			});
-		}
-	});
-	*/
-}
-
-// inital add eventListener to all anchors
-addWarnToNewAnchors();
-
-// monitor changes 
-(new MutationObserver(addWarnToNewAnchors)).observe(document.body, { attributes: false, childList: true, subtree: true }); 
-
+document.addEventListener('click', warn,false);
